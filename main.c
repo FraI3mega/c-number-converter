818873def55d7ed438d531f28bc1441d9dcb6172
FraI3mega <projeapp2@gmail.com>
2026-08-15 13:57:40 +0200

    feat(main): Add basic logic

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int findIndex(char symbol);
+const char table[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // length 36
 
 int main() {
+
   printf("|=========================|\n");
   printf("| Number System converter |\n");
   printf("|=========================|\n");
@@ -9,7 +14,39 @@ int main() {
   printf("Choose 1,2,3 or 4: ");
   scanf("%d",&operation);
 
-  printf("Chosen operation: %d\n",operation);
+  switch (operation) {
+    case 1:
+      printf("1: Convert from input to decimal\n");
+      char input[30];
+      printf("Type in the input number: ");
+      scanf("%s", input);
+      printf("%d", findIndex('A'));
+      break;
+    case 2:
+      printf("2: Convert from decimal to other\n");
+      break;
+    case 3:
+      printf("3: Convert from any to any\n");
+      break;
+    case 4:
+      printf("4: Quit\n");
+      return 0;
+      break;
+    default:
+      printf("Choose a correct operation\n");
+  }
 
   return 0;
+}
+
+int findIndex(char symbol) {
+  int index ;
+  int i;
+
+  for (i = 0;i<36;i++) {
+    if (symbol == table[i]) {
+      return i;
+    }
+  }
+  exit(1);
 }
